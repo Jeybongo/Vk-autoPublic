b3154a4d3b24f9210f468359a691f969477fa500
Nikita Goryushin <jeybongo@gmail.com>
2016-12-02 11:53:58 +0300

    Event for photo post added.

--- vsproject/VkWorker.cs
+++ vsproject/VkWorker.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VkPoster
 {
+    //delegate for vk post event
+    delegate void PhotoPosted(FileInfo photo);
+
     class VkWorker
     {
-        String _token;
-        String _publicId;
+        public event PhotoPosted onPost;
+
+        private String _token;
+        private String _publicId;
 
         //for authorization
         public bool auth()
@@ -24,7 +30,7 @@ namespace VkPoster
         }
 
         //post photo with path to public
-        public bool post(String path)
+        public bool post(FileInfo fileInfo)
         {
             return true;
         }
